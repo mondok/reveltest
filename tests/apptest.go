e238e2f362b6cae6adf1ea8110f64c44dfc1a13a
Matt Mondok <matt.mondok@gmail.com>
2015-04-16 07:22:07 -0700

    Added PostForm test

--- tests/apptest.go
+++ tests/apptest.go
@@ -1,6 +1,10 @@
 package tests
 
-import "github.com/revel/revel/testing"
+import (
+	"net/url"
+
+	"github.com/revel/revel/testing"
+)
 
 type AppTest struct {
 	testing.TestSuite
@@ -10,8 +14,17 @@ func (t *AppTest) Before() {
 	println("Set up")
 }
 
-func (t *AppTest) TestThatIndexPageWorks() {
+func (t *AppTest) TestIndex() {
 	t.Get("/")
+	t.AssertOk()
+	t.AssertContentType("text/html; charset=utf-8")
+}
+
+func (t *AppTest) TestPerson() {
+	v := url.Values{}
+	v.Set("person.Name", "Ava")
+	v.Add("person.Age", "11")
+	t.PostForm("/Name", v)
 	t.AssertOk()
 	t.AssertContentType("text/html; charset=utf-8")
 }
