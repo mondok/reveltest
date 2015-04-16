db64eced5e0ee5bdca02d31e3bdeba8ea7b758a6
Matt Mondok <matt.mondok@gmail.com>
2015-04-16 07:03:48 -0700

    Added basic form POST

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -6,8 +6,19 @@ type App struct {
 	*revel.Controller
 }
 
+type You struct {
+	Name string
+	Age  int
+}
+
 func (c App) Index() revel.Result {
 	title := "Index"
 	name := "Page 1"
 	return c.Render(title, name)
+}
+
+func (c App) Name() revel.Result {
+	var you You
+	c.Params.Bind(&you, "you")
+	return c.Render(you)
 }
